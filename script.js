525ac1ffe3fa6638b87c7f81725265e19c2917ca
Encryptix <feboketamaratare@gmail.com>
2025-12-10 18:43:55 +0100

    Add event listener for title click alert

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+console.log("JavaScript is connected!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector("h1");
+    title.addEventListener("click", () => {
+        alert("Welcome to the Encryptixx Blog!");
+    });
+});
 
